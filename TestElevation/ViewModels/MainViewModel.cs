2abed6311bc559a982d5b5b95e4aaa1b72c93149
shinta0806 <shinta.0806@gmail.com>
2024-04-24 18:40:44 +0900

    昇格処理導入。

--- TestElevation/ViewModels/MainViewModel.cs
+++ TestElevation/ViewModels/MainViewModel.cs
@@ -1,10 +1,71 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Diagnostics;
+using System.Security.Principal;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
 
 namespace TestElevation.ViewModels;
 
 public partial class MainViewModel : ObservableRecipient
 {
-    public MainViewModel()
-    {
-    }
+	public MainViewModel()
+	{
+		if (IsAdminRole())
+		{
+			ButtonElevateVisibility = Visibility.Collapsed;
+			TextBlockElevatedVisibility = Visibility.Visible;
+		}
+		else
+		{
+			ButtonElevateVisibility = Visibility.Visible;
+			TextBlockElevatedVisibility = Visibility.Collapsed;
+		}
+		ButtonElevateClickedCommand = new RelayCommand(ButtonElevateClicked);
+	}
+
+	private Visibility _ButtonElevateVisibility;
+	public Visibility ButtonElevateVisibility
+	{
+		get => _ButtonElevateVisibility;
+		set => SetProperty(ref _ButtonElevateVisibility, value);
+	}
+
+	private Visibility _textBlockElevatedVisibility;
+	public Visibility TextBlockElevatedVisibility
+	{
+		get => _textBlockElevatedVisibility;
+		set => SetProperty(ref _textBlockElevatedVisibility, value);
+	}
+
+	#region ButtonElevate
+	public RelayCommand ButtonElevateClickedCommand
+	{
+		get;
+	}
+
+	private void ButtonElevateClicked()
+	{
+		try
+		{
+			ProcessStartInfo startInfo = new()
+			{
+				UseShellExecute = true,
+				FileName = Path.ChangeExtension(Environment.GetCommandLineArgs()[0], ".exe"),
+				Verb = "runas",
+			};
+			Process.Start(startInfo);
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine("err: " + ex.Message);
+		}
+	}
+	#endregion
+
+	private static Boolean IsAdminRole()
+	{
+		WindowsPrincipal windowsPrincipal = new(WindowsIdentity.GetCurrent());
+		return windowsPrincipal.IsInRole(WindowsBuiltInRole.Administrator);
+	}
+
 }
