70f17b1f9413fe397a74ada97586e769d3379bb7
shinta0806 <shinta.0806@gmail.com>
2024-04-24 18:15:18 +0900

    プロジェクト ファイルを追加します。

---
+++ TestElevation/Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace TestElevation.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ TestElevation/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+using TestElevation.ViewModels;
+
+namespace TestElevation.Views;
+
+public sealed partial class MainPage : Page
+{
+    public MainViewModel ViewModel
+    {
+        get;
+    }
+
+    public MainPage()
+    {
+        ViewModel = App.GetService<MainViewModel>();
+        InitializeComponent();
+    }
+}
